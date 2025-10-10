cb4eab2b7a8f6afba82d6c15f50754c4c4778722
is61b <is61blp3i@gmail.com>
2025-10-10 10:32:03 +0700

    form logika

---
+++ form-logika.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login Sederhana</title>
+</head>
+<body>
+    <h1>Form Login Sederhana</h1>
+    <form action="">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="submit">Log In</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
