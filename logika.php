54b5f48ed061db293956a8f7e88aa2309a02364e
is61b <is61blp3i@gmail.com>
2025-10-10 09:49:34 +0700

    if part 2

--- logika.php
+++ logika.php
@@ -14,19 +14,46 @@
     # OPERATOR PERBANDINGAN (== != > < >= <=)
     # OPERATOR LOGIKA ( AND OR) (&& ||)
 
-    $nilai = 40;
 
-    if($nilai > 50){
-        echo "Kamu Lulus";
-        if($nilai >90){
-            echo ", Nilai A";
-        }
+    $username = "admin";
+    $password = "123456";
+
+    if($username == "admin" && $password == "123456"){
+        echo "Login Berhasil";
+    }elseif($username == "admin" && $password != "123456"){
+        echo "Password Salah";
+    }elseif($username != "admin" && $password == "123456"){
+        echo "Username Salah";
     }else{
-        echo "Kamu Tidak Lulus";
-        if($nilai >30){
-            echo ", Harus Ikut Remedial";
-        }
+        echo "Login Gagal";
     }
+
+    // $nama = "Budi";
+    // $nilai = 20;
+
+    // if($nilai >= 70){
+    //     echo "Selamat $nama, Kamu Lulus";
+    // }elseif($nilai >= 40){
+    //     echo "Hai, $nama. Kamu harus ikut remedial";
+    // }elseif($nilai >= 10){
+    //     echo "Maaf $nama, kamu Tidak Lulus";
+    // }else{
+    //     echo "Hati-hati $nama, Kamu Akan di DO jika tidak memperbaiki nilai di MK lainnya";
+    // }
+
+
+
+    // if($nilai > 50){
+    //     echo "Kamu Lulus";
+    //     if($nilai >90){
+    //         echo ", Nilai A";
+    //     }
+    // }else{
+    //     echo "Kamu Tidak Lulus";
+    //     if($nilai >30){
+    //         echo ", Harus Ikut Remedial";
+    //     }
+    // }
     ?>
 </body>
 </html>
