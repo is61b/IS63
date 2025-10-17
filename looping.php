6b1b9385ec4cffd1012fd6c29fb5f73b57576176
is61b <is61blp3i@gmail.com>
2025-10-17 09:50:45 +0700

    while & do while

--- looping.php
+++ looping.php
@@ -1,7 +1,23 @@
 <?php
 ### FOR ###
-for($i=1;$i<=50;$i--){
+for($i=1;$i<=10;$i++){
     echo "Mahasiswa Ke-$i <br>";
 }
+
+echo "<hr>";
+### WHILE ###
+$awal = 1;
+while($awal <= 10){
+    echo "Mahasiswa Ke-$awal<br>";
+    $awal++;
+}
+
+echo "<hr>";
+### DO WHILE ###
+$dw = 1;
+do{
+    echo "Mahasiswa Ke-$dw <br>";
+    $dw++;
+}while($dw <= 10);
 
 ?>
