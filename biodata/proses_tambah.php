e4e1ff13ab1fea3be5e646cbd55229c60157d3f3
is61b <is61blp3i@gmail.com>
2025-10-31 10:55:38 +0700

    insert

--- biodata/proses_tambah.php
+++ biodata/proses_tambah.php
@@ -1,9 +1,27 @@
 <?php
     #1. Meng-koneksikan PHP ke MySQL
+    include("koneksi.php");
 
     #2. Mengambil Value dari Form Tambah
+    $nama = $_POST['nama'];
+    $nisn = $_POST['nisn'];
+    $tp_lahir = $_POST['tp_lahir'];
+    $tg_lahir = $_POST['tg_lahir'];
+    $alamat = $_POST['alamat'];
+    $email = $_POST['email'];
+    $jk = $_POST['jk'];
+    $jur = $_POST['jur'];
 
     #3. Query Insert (proses tambah data)
+    $query = "INSERT INTO biodata (nama,nisn,tp_lahir,tg_lahir,alamat,email,jk,jur) 
+    VALUES ('$nama','$nisn','$tp_lahir','$tg_lahir','$alamat','$email','$jk','$jur')";
+
+    $tambah = mysqli_query($koneksi,$query);
 
     #4. Jika Berhasil triggernya apa? (optional)
+    if($tambah){
+        header("location:index.php");
+    }else{
+        echo "Data Gagal ditambah";
+    }
 ?>
